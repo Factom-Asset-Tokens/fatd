91b003f6a154121caa13327a8fe07dd6c31c7211
Adam S Levy <adam@aslevy.com>
2018-11-23 11:49:26 -0900

    docs(factom,fat0): Improve top level package documentation

--- fat0/doc.go
+++ fat0/doc.go
@@ -1,4 +1,4 @@
 // Package fat0 provides data types corresponding to valid FAT-0 entries for
 // Transaction and Issuance as well as methods for validating the structure and
-// content for the factom entry.
+// content of the factom entry.
 package fat0
